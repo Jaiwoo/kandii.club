bde375170b157f4a7ee8a3695e0fb43f6cc9d999
Jon Wood <JonWood@Jon-Woods-MacBook-Air.local>
2022-02-08 19:36:32 -0500

    new repo, initial commit

---
+++ src/App.js
@@ -0,0 +1,200 @@
+import "./App.css";
+import jacob from "./img/jacob.jpg";
+import jaiwoo from "./img/jaiwoo.jpg";
+import nekog from "./img/nekog.jpg";
+import cruz from "./img/cruz.jpg";
+import actii from "./img/actii.jpg";
+import ariel from "./img/ariel.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <header className="App-header">
+        <p>Welcome to Jacob's</p>
+        <p id="Header-title">kandii.club</p>
+      </header>
+
+      <a
+        href="https://www.instagram.com/vtjacob/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img className="Header-img" src={jacob} alt="jacob"></img>
+      </a>
+
+      <p className="Section-header">
+        Sat. Feb. 19th 2022 <br></br>Secret Loft NYC <br></br> 10p - 4a
+      </p>
+
+      {/* Text Section */}
+      <div className="Text-container">
+        <p>
+          Before our boy moves forward into his 30s we're throwing it back for
+          an early 2010's-style Rave and YOUR'RE invited!
+        </p>
+        <p>
+          Think kandi bracelets, fuzzy leg warmers, outrageous costumes, and all
+          neon everything. Get ready for some Mainstage Mania!
+        </p>
+        <p>
+          We'll be at Secret Loft in Manhattan with some of our beloved DJ
+          friends spinning the finest bangers of the era + full bar, snacks &
+          some fun surprises!
+        </p>
+        <p>If this sounds up your alley then get on our guest list here!</p>
+        <a
+          id="text-link"
+          href="https://forms.gle/9JkAxaZRJatVpBLfA"
+          rel="noreferrer"
+          target="_blank"
+        >
+          RSVP
+        </a>
+        <p>
+          Any donatations to our Rave Prep are much appreciated & we promise it
+          will be put to good use.
+        </p>
+        <p>
+          <a
+            id="text-link"
+            href={"https://urlgeni.us/venmo/kandiclub"}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Venmo
+          </a>
+        </p>
+      </div>
+
+      <p className="Section-header">With Special Performances By:</p>
+      {/* Ariel */}
+      <a
+        href="https://www.instagram.com/ariel.ist/"
+        rel="noreferrer"
+        target="_blank"
+        className="Talent-card"
+      >
+        <img className="Talent-img" id="ariel-img" src={ariel} alt=""></img>
+
+        <p>Ariel Iasevoli</p>
+      </a>
+
+      {/* Talent Section */}
+      <p className="Section-header">Your Kandii Kurators</p>
+      {/* Jaiwoo */}
+      <a
+        href="https://www.instagram.com/djaiwoo/"
+        rel="noreferrer"
+        target="_blank"
+        className="Talent-card"
+      >
+        <img className="Talent-img" id="jaiwoo-img" src={jaiwoo} alt=""></img>
+
+        <p>Jaiwoo</p>
+      </a>
+
+      {/* Cruz Cruz */}
+      <a
+        href="https://www.instagram.com/cruzcruzofficial/"
+        rel="noreferrer"
+        target="_blank"
+        className="Talent-card"
+      >
+        <img className="Talent-img" id="cruz-img" src={cruz} alt=""></img>
+        <p>Cruz Cruz</p>
+      </a>
+
+      {/* Act II */}
+      <a
+        href="https://www.instagram.com/holleratyourboy/"
+        rel="noreferrer"
+        target="_blank"
+        className="Talent-card"
+      >
+        <img className="Talent-img" id="actii-img" src={actii} alt=""></img>
+        <p>Act II</p>
+      </a>
+
+      {/* Neko G */}
+      <a
+        href="https://www.instagram.com/nekogisadj/"
+        rel="noreferrer"
+        target="_blank"
+        className="Talent-card"
+      >
+        <img className="Talent-img" id="nekog-img" src={nekog} alt=""></img>
+        <p>Neko G</p>
+      </a>
+
+
+      {/* Kandii Viibe Check */}
+      <p className="Section-header">Our Kandii Viibes</p>
+
+      <div className="Genre-container">
+        <p>Mainstage Mania</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Big-Room Bonanza</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>In Your Face Electro</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Complextro Killers</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Half-Time Havoc</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Peak Time Pop</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Euro-Trance Euphoria</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>Mid-Tempo Madness</p>
+      </div>
+
+      <div className="Genre-container">
+        <p>
+          Kandii Klassics <br></br>
+          sing-a-long
+        </p>
+      </div>
+
+      {/* Spotify Embed */}
+      <p className="Section-header">Get Hype!</p>
+      <iframe
+        title="spotify"
+        src="https://open.spotify.com/embed/playlist/7qyfH51AhGM5YEVjEgBtMF?utm_source=generator"
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+
+      {/* Google Map Embed */}
+      <p className="Section-header">Location</p>
+      <iframe
+        title="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.0753001586595!2d-74.0005315840398!3d40.738368579329155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259aaebe3a5fb%3A0xfdaf0dfc0a2978a6!2sSecret%20Loft!5e0!3m2!1sen!2sus!4v1642290346333!5m2!1sen!2sus"
+        width="100%"
+        height="450"
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+      <footer>Created by Jaiwoo</footer>
+    </div>
+  );
+}
+
+export default App;
